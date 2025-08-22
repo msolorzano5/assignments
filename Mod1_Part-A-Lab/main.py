90c8020815df9e9d165392048c6ed5f9aeae8ff0
msolorzano5 <msolorzano5@mail.pima.edu>
2025-08-21 17:29:15 -0700

    Only needs the TTS logic

--- Mod1_Part-A-Lab/main.py
+++ Mod1_Part-A-Lab/main.py
@@ -1,19 +1,45 @@
 # task: Display a personalized greeting based on the user’s name and the time of day.
+from datetime import datetime
 
-import time
+def say(text):
+    # replace with actual TTS logic
+    print(text)
 
-def main():
-    # check if we know the user name
+def get_date_time():
+    dated = datetime.now().date()
+    time = datetime.now().time()
+    date = dated.strftime("%B %d, %Y")
+    day = dated.strftime("%A")
+    time = time.strftime("%I:%M %p")
+    return date,day,time
+
+def get_info(info):
+    with open("data.txt", 'w') as data_file:
+        if info == "name":
+            name = input("What is your name? ")
+            data_file.seek(0)
+            data_file.write(name)
+
+def update_info():
     with open("data.txt", 'r') as d:
-        d = [line.strip() for line in d]
-        
-    exec(d)
-    print(user_name)
-
-
-
-
-
+        data = [line.strip() for line in d]
+    return data
 
 if __name__ == "__main__":
-    main()
+    # load and update data
+    d = update_info()
+    # check if we know the user's name
+    user_name = d[0]
+    if user_name == 'None':
+         get_info("name")
+         d = update_info()
+         user_name = d[0]
+
+    ## start building the string to say
+    date, day, time = get_date_time()
+    to_say = f"Hello {user_name}. The date is {date}, it is {day}, and the time is {time}"
+    say(to_say)
+
+
+
+
